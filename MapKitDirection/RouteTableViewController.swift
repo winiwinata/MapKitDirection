03ca8d9297d8ce33ab9b66e2e72ec02acda95a29
wini <winiwinata90@gmail.com>
2020-12-10 11:55:26 +0800

    Initial Commit

---
+++ MapKitDirection/RouteTableViewController.swift
@@ -0,0 +1,52 @@
+//
+//  RouteTableViewController.swift
+//  MapKitDirection
+//
+//  Created by Simon Ng on 9/11/2017.
+//  Copyright © 2017 AppCoda. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class RouteTableViewController: UITableViewController {
+
+    var routeSteps = [MKRoute.Step]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // Return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of rows
+        return routeSteps.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        // Configure the cell...
+        cell.textLabel?.text = routeSteps[indexPath.row].instructions
+        
+        return cell
+    }
+
+}
